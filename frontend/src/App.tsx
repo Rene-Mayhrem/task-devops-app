0986100f3711c74fa86a7fc62dd09e68b0e58402
mayhrem <renecruz.0711@gmail.com>
2025-01-19 12:57:43 -0600

    Ignoring files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './components/Home';
-import About from './components/About';
-import NotFound from './components/NotFound';
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <div>
-        <h1>DevOps CRUD Application</h1>
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/about" component={About} />
-          <Route component={NotFound} />
-        </Switch>
-      </div>
-    </Router>
+    <div>
+      <h1>Holaaaaa</h1>
+    </div>
   );
 };
 
